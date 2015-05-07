4c5bca12ccd48b4678016171426585a15d1025a2
Pim De Witte <pim@whitespell.com>
2015-05-07 10:16:53 -0400

    test

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 git add --all
 git commit -m $1
 git push origin master
-ssh 104.197.10.86 "cd /var/www/whitespell.com/htdocs && git pull --force origin master"
+ssh 104.197.10.86 "cd /usr/share/whitespell.com && git pull --force origin master"
+ssh 104.197.10.86 "cp /usr/share/whitespell.com/www/* /var/www/whitespell.com/htdocs"
