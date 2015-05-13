16d4d36ad6f22f3d3a8276bdad14defc9f86fc4c
Pim De Witte <pim@whitespell.com>
2015-05-13 11:40:43 -0400

    new hsotnaem deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Starting deployment";
 ssh website-internal.whitespell.com "sudo mkdir -p /var/www/whitespell.com && sudo chmod -R 777 /var/www/whitespell.com";
-scp -r www/* website.whitespell.com:/var/www/whitespell.com;
+scp -r www/* website-internal.whitespell.com:/var/www/whitespell.com;
 ssh website-internal.whitespell.com "sudo chmod -R 755 /var/www/whitespell.com";
 echo "Deployment Successful";
 
