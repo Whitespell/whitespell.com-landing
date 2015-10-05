9a40dd18dfa8fa1a83a893428c8eb9443d0c3cdc
Koen Vendrik <k.vendrik@gmail.com>
2015-10-05 20:55:55 +0200

    reorganises team page

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 cat ~/.ssh/id_rsa.pub;
 echo "Starting deployment";
 ssh website-internal.whitespell.com "sudo mkdir -p /var/www/whitespell.com && sudo chmod -R 777 /var/www/whitespell.com";
-scp -r www/* website-internal.whitespell.com:/var/www/whitespell.com;
+rsync -av www/ website-internal.whitespell.com:/var/www/whitespell.com;
 ssh website-internal.whitespell.com "sudo chmod -R 755 /var/www/whitespell.com";
 echo "Deployment Successful";
 
